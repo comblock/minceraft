3583edfa180ab666c8e476637403f7f9c27ed0bc
comblock <81411315+comblock@users.noreply.github.com>
2022-08-01 16:39:41 +0200

    Position fields are now public

--- minceraft/src/net/types/mod.rs
+++ minceraft/src/net/types/mod.rs
@@ -4,13 +4,14 @@ pub use crate::inv::{
     item::{Item, Itemstack},
     Slot,
 };
-use anyhow::{Result, bail};
+use anyhow::{bail, Result};
 pub use nbt::Blob as Nbt;
 use std::{
     borrow::Cow,
     convert::{TryFrom, TryInto},
     io,
-    num::TryFromIntError, marker::PhantomData,
+    marker::PhantomData,
+    num::TryFromIntError,
 };
 pub use uuid::Uuid;
 
@@ -272,9 +273,9 @@ impl Decoder for Uuid {
 
 #[derive(Debug, Clone)]
 pub struct Position {
-    x: i32,
-    y: i32,
-    z: i32,
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
 }
 
 impl Encoder for Position {
@@ -291,29 +292,27 @@ impl Decoder for Position {
 }
 
 /// An Array of type T which is prefixed by type U
-// TODO: Optimise
-// The following code was heavily inspired by 
-// https://github.com/feather-rs/feather/blob/2f99d76aaad022e65550c88594b7b9b259503c16/feather/protocol/src/io.rs 
+// The following code was heavily inspired by
+// https://github.com/feather-rs/feather/blob/2f99d76aaad022e65550c88594b7b9b259503c16/feather/protocol/src/io.rs
 // which is under the apache 2.0 license
 // ----------------------------------------------------------------------------
-pub struct Array<'a, T, U> (pub Cow<'a, [T]>, PhantomData<U>)
+pub struct Array<'a, T, U>(pub Cow<'a, [T]>, PhantomData<U>)
 where
     [T]: ToOwned<Owned = Vec<T>>;
 
-impl<'a, T, U> Array<'a, T, U> 
+impl<'a, T, U> Array<'a, T, U>
 where
-    [T]: ToOwned<Owned = Vec<T>>
+    [T]: ToOwned<Owned = Vec<T>>,
 {
-    const MAX_LENGTH: usize = 2^20;
+    const MAX_LENGTH: usize = 2 ^ 20;
 }
 
-
-impl<'a, T, U> Encoder for Array<'a, T, U> 
-where 
+impl<'a, T, U> Encoder for Array<'a, T, U>
+where
     T: Encoder,
     U: Encoder + TryFrom<usize>,
     U::Error: std::error::Error + Send + Sync + 'static,
-    [T]: ToOwned<Owned = Vec<T>>
+    [T]: ToOwned<Owned = Vec<T>>,
 {
     fn write_to(&self, w: &mut impl io::Write) -> Result<()> {
         let len = U::try_from(self.0.len())?;
@@ -325,12 +324,12 @@ where
     }
 }
 
-impl<'a, T, U> Decoder for Array<'a, T, U> 
-where 
+impl<'a, T, U> Decoder for Array<'a, T, U>
+where
     T: Decoder,
     U: Decoder + TryInto<usize>,
     U::Error: std::error::Error + Send + Sync + 'static,
-    [T]: ToOwned<Owned = Vec<T>>
+    [T]: ToOwned<Owned = Vec<T>>,
 {
     fn read_from(r: &mut impl io::Read) -> Result<Self> {
         let len: usize = U::read_from(r)?.try_into()?;
@@ -346,20 +345,20 @@ where
     }
 }
 
-impl<'a, T, U> From::<Array::<'a, T, U>> for Vec<T> 
+impl<'a, T, U> From<Array<'a, T, U>> for Vec<T>
 where
-    [T]: ToOwned<Owned = Vec<T>>
+    [T]: ToOwned<Owned = Vec<T>>,
 {
-    fn from(arr: Array::<'a, T, U>) -> Self {
+    fn from(arr: Array<'a, T, U>) -> Self {
         arr.0.into_owned()
     }
 }
 
-impl<'a, T, U> From::<Vec::<T>> for Array<'a, T, U> 
-where 
-    [T]: ToOwned<Owned = Vec<T>>
+impl<'a, T, U> From<Vec<T>> for Array<'a, T, U>
+where
+    [T]: ToOwned<Owned = Vec<T>>,
 {
-    fn from(vec: Vec::<T>) -> Self {
+    fn from(vec: Vec<T>) -> Self {
         Self(Cow::Owned(vec), PhantomData)
     }
 }
@@ -377,7 +376,6 @@ pub type VarIntPrefixedArray<'a, T> = Array<'a, T, VarInt>;
 pub type ShortPrefixedArray<'a, T> = Array<'a, T, u8>;
 pub type ByteArray<'a, U> = Array<'a, u8, U>;
 pub type BitSet<'a, U> = Array<'a, u8, U>;
-
 
 pub struct LengthInferredByteArray<'a>(pub Cow<'a, [u8]>);
 
@@ -451,6 +449,21 @@ impl<T: Item, U: Enchant> Decoder for Slot<T, U> {
                 })
             }
         })
+    }
+}
+
+// TODO: Properly implement a particle data type
+pub struct Particle;
+
+impl Encoder for Particle {
+    fn write_to(&self, _w: &mut impl io::Write) -> Result<()> {
+        todo!()
+    }
+}
+
+impl Decoder for Particle {
+    fn read_from(_r: &mut impl io::Read) -> Result<Self> {
+        todo!()
     }
 }
 
