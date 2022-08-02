c9c042b811e419c59fae784a511d3f496967a1e9
comblock <81411315+comblock@users.noreply.github.com>
2022-08-02 16:31:00 +0200

    continued work on p47

--- minceraft/src/p47/enums.rs
+++ minceraft/src/p47/enums.rs
@@ -60,3 +60,42 @@ def_enum! {
         5 = MagicalCriticalEffect,
     }
 }
+
+def_enum! {
+    WindDirection(u8) {
+        0 = North,
+        1 = West,
+        2 = South,
+        3 = East,
+    }
+}
+
+def_enum! {
+    // TODO: Figure out what's up with the duplicate 10
+    EntityStatus(i8) {
+        1 = ResetMobSpawnMinecartTimerOrRabbitJumpAnimation, //TODO: Come up with a better name for this mess
+        2 = LivingEntityHurt,
+        3 = LivingEntityDead,
+        4 = IronGolemThrowingUpArms,
+        // where is 5?
+        6 = TamingSpawnHeartParticles,
+        7 = TamingSpawnSmokeParticles,
+        8 = WolfShakingWater,
+        9 = PlayerEatingAcceptedByServer,
+        10 = SheepEatingGrassOrPlayTntIgniteSound, // since there was a duplicate 10 I combined these
+        // 10 = PlayTntIgniteSound, // duplicate 10?
+        11 = IronGolemHandingOverRose,
+        12 = VillagerMatingSpawnHeartParticles,
+        13 = SpawnAngryVillagerParticles,
+        14 = SpawnHappyVillagerParticles,
+        15 = WitchSpawnMagicParticles,
+        16 = PlayZombieConvertingIntoVillagerSound,
+        17 = FireWorkExploding,
+        18 = AnimalInLoveSpawnHeartParticles,
+        19 = ResetSquidRotation,
+        20 = SpawnExplosionParticle,
+        21 = PlayGuardianSound,
+        22 = EnableReducedDebugInfo,
+        23 = DisableReducedDebugInfo,
+    }
+}
--- minceraft/src/p47/packets/play/clientbound.rs
+++ minceraft/src/p47/packets/play/clientbound.rs
@@ -8,7 +8,7 @@ packets! {
         id VarInt
     },
     JoinGame(0x01) {
-        entity_id i32;
+        eid i32;
         gamemode u8;
         dimension Dimension;
         difficulty u8;
@@ -25,7 +25,7 @@ packets! {
         time_of_day i64;
     }
     EntityEquipment(0x04) {
-        entity_id VarInt;
+        eid VarInt;
         slot EquipmentSlot;
         item Slot;
     }
@@ -55,15 +55,15 @@ packets! {
         slot i8; // 0-8
     }
     UseBed(0x0A) {
-        entity_id VarInt;
+        eid VarInt;
         location Position;
     }
     Animation(0x0B) {
-        entity_id VarInt;
+        eid VarInt;
         animation AnimationId;
     }
     SpawnPlayer(0x0C) {
-        entity_id VarInt;
+        eid VarInt;
         player_uuid Uuid;
         x i32;
         y i32;
@@ -72,5 +72,191 @@ packets! {
         pitch Angle;
         current_item i16; // 0 for no item instead of -1
         metadata EntityMetaData;
+    }
+    CollectItem(0x0D) {
+        collected_eid VarInt;
+        collector_eid VarInt;
+    }
+    SpawnObject(0x0E) {
+        eid VarInt;
+        ty u8;
+        x i32;
+        y i32;
+        z i32;
+        pitch Angle;
+        yaw Angle;
+        inner SpawnObjectInner;
+    }
+    SpawnMob(0x0F) {
+        eid VarInt;
+        ty u8;
+        x i32;
+        y i32;
+        z i32;
+        yaw Angle;
+        pitch Angle;
+        head_pitch Angle;
+        velocity_x i16;
+        velocity_y i16;
+        velocity_z i16;
+        metadata EntityMetaData;
+    }
+    SpawnPainting(0x10) {
+        eid VarInt;
+        title String;
+        location Position;
+        direction WindDirection;
+    }
+    SpawnExperienceOrb(0x11) {
+        eid VarInt;
+        x i32;
+        y i32;
+        z i32;
+        count i16;
+    }
+    EntityVelocity(0x11) {
+        eid VarInt;
+        velocity_x i16;
+        velocity_y i16;
+        velocity_z i16;
+    }
+    DestroyEntities(0x13) {
+        eids VarIntPrefixedArray<u8>
+    }
+    Entity(0x14) {
+        eid VarInt;
+    }
+    EntityRelativeMove(0x15) {
+        eid VarInt;
+        delta_x i8;
+        delta_y i8;
+        delta_z i8;
+        on_ground bool;
+    }
+    EntityLook(0x16) {
+        eid VarInt;
+        yaw Angle;
+        pitch Angle;
+        on_ground bool;
+    }
+    EntityLookAndRelativeMove(0x17) {
+        eid VarInt;
+        delta_x i8;
+        delta_y i8;
+        delta_z i8;
+        yaw Angle;
+        pitch Angle;
+        on_ground bool;
+    }
+    EntityTeleport(0x18) {
+        eid VarInt;
+        x i32;
+        y i32;
+        z i32;
+        yaw Angle;
+        pitch Angle;
+        on_ground bool;
+    }
+    EntityHeadLook(0x19) {
+        eid VarInt;
+        head_yaw Angle;
+    }
+    UpdateEntityStatus(0x1A) {
+        eid i32;
+        status EntityStatus;
+    }
+    AttachEntity(0x1B) {
+        eid i32;
+        vehicle_eid i32; // set to -1 to detach
+        leash bool; // if true leashes the entity to the vehicle
+    }
+    UpdataEntityMetaData(0x1C) {
+        eid VarInt;
+        metadata EntityMetaData;
+    }
+    EntityEffect(0x1D) {
+        eid VarInt;
+        effect i8; // TODO: add a propper effect type (can be generated with https://github.com/PrismarineJS/minecraft-data/blob/master/data/pc/1.8/effects.json)
+        amplifier i8; // Notchian client displas effect level as amplifier + 1 + shouldn't this be u8???
+        duration VarInt; // Seconds
+        hide_particles bool;
+    }
+    RemoveEntityEffect(0x1E) {
+        eid VarInt;
+        effect i8;
+    }
+    SetExperience(0x1F) {
+        experience_bar f32; // between 0 and 1
+        level VarInt;
+        total_experience VarInt;
+    }
+    //TODO: finish entity properties packet, this includes making a proper attribute type which can be generated with https://github.com/PrismarineJS/minecraft-data/blob/master/data/pc/1.8/attributes.json
+    EntityProperties(20) {
+        eid VarInt;
+        //...
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SpawnObjectInner {
+    pub data: i32,
+    pub velocity: Option<SpawnObjectVelocity>
+
+}
+
+impl Encoder for SpawnObjectInner {
+    fn write_to(&self, w: &mut impl std::io::Write) -> anyhow::Result<()> {
+        self.data.write_to(w)?;
+        if self.data != 0 {
+            self.velocity.as_ref().unwrap().write_to(w)?;
+        }
+        Ok(())
+    }
+}
+
+impl Decoder for SpawnObjectInner {
+    fn read_from(r: &mut impl std::io::Read) -> anyhow::Result<Self> {
+        let data = i32::read_from(r)?;
+        let velocity = if data != 0 {
+            Some(SpawnObjectVelocity {
+                x: i16::read_from(r)?,
+                y: i16::read_from(r)?,
+                z: i16::read_from(r)?
+            })
+        } else {
+            None
+        };
+        Ok(
+            Self {
+                data,
+                velocity
+            }
+        )
+    }
+}
+
+// TODO: Add a encoder and decoder derive and use that instead
+#[derive(Debug, Clone)]
+pub struct SpawnObjectVelocity {
+    pub x: i16,
+    pub y: i16,
+    pub z: i16, 
+}
+
+impl Encoder for SpawnObjectVelocity {
+    fn write_to(&self, w: &mut impl std::io::Write) -> anyhow::Result<()> {
+        self.x.write_to(w)?;
+        self.y.write_to(w)?;
+        self.z.write_to(w)
+    }
+}
+
+impl Decoder for SpawnObjectVelocity {
+    fn read_from(r: &mut impl std::io::Read) -> anyhow::Result<Self> {
+        Ok(Self {
+            x: i16::read_from(r)?,
+            y: i16::read_from(r)?,
+            z: i16::read_from(r)?,
+        })
     }
 }
